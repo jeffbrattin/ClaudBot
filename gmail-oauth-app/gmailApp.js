e8d6111b6281a47eac3a21042fc3adcd7329f738
Jeff Brattin <jeffbrattin@gmail.com>
2026-01-28 11:39:05 -1000

    main

--- gmail-oauth-app/gmailApp.js
+++ gmail-oauth-app/gmailApp.js
@@ -63,6 +63,6 @@ async function getUserProfile(tokens) {
 }
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
